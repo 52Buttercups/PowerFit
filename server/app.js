9391f4e9d5a3f5fb0810c7e3d1a9ba95759a761b
Tom <tomshibley@gmail.com>
2021-05-22 11:46:40 -0700

    Fixed broken destructure of port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyparser = require('body-parser');
 
-const { PORT } = process.env || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
