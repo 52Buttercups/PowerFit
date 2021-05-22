17698914fe09a6efbb237415207e19066626895a
Tom <tomshibley@gmail.com>
2021-05-22 15:20:57 -0700

    Added styling for login form

---
+++ client/src/components/welcome/SignUp.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from './signUp.module.css';
+
+const SignUp = () => {
+  const [formData, setFromData] = useState({
+    username: '',
+    password: '',
+  });
+  return (
+    <form className={styles.form}>
+      <label className={styles.inputLabel} htmlFor="username">
+        Username
+      </label>
+      <input className={styles.input} id="username" type="text" name="username" value={formData.username} placeholder="username" />
+      <div className={styles.formError}>
+        {/* <p>error</p> */}
+      </div>
+      <label className={styles.inputLabel} htmlFor="password">
+        Password
+      </label>
+      <input className={styles.input} id="password" type="password" name="password" value={formData.password} placeholder="password" />
+      <div className={styles.formError}>
+        {/* <p>error</p> */}
+      </div>
+      <button type="submit">Signup</button>
+      <p className={styles.formSwitch}>
+        Need to
+        <span className={styles.formLink}>Login</span>
+        ?
+      </p>
+    </form>
+  );
+};
+
+export default SignUp;
--- client/src/components/welcome/Welcome.jsx
+++ client/src/components/welcome/Welcome.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './welcome.module.css';
+import SignUp from './SignUp';
 
 const Welcome = () => (
   <div className={styles.container}>
-    <div className={styles.formContainer}>form</div>
+    <div className={styles.formContainer}>
+      <SignUp />
+    </div>
     <div className={styles.heroText}>
       <h2>Sign Up or Log In to start working out</h2>
       <p>Some text here</p>
