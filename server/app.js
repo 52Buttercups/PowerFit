25aed11334ceb1a10a7a205ab62d75f9c07705b3
jyxsul <jonn.sullins@gmail.com>
2021-05-22 12:41:26 -0500

    express server setup

--- server/app.js
+++ server/app.js
@@ -1,41 +1,23 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+require('dotenv').config();
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+const bodyparser = require('body-parser');
+const PORT = 5000;
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+app.get('/', async (req, res) => {
+  try {
+    res.status(200).json({message: 'Hello from Buttercups Server'});
+  } catch(err) {
+    console.error(err);
+  }
 });
 
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = app;
