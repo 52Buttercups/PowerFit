4cdb996c98698e457b92764260be3d19b2323906
Alex <alex.garay94@gmail.com>
2021-05-24 15:26:22 -0700

    Pulling latest changes

--- server/app.js
+++ server/app.js
@@ -1,56 +1,29 @@
-const express = require("express");
-const logger = require("morgan");
-const bodyparser = require("body-parser");
-const passport = require("passport"),
-  LocalStrategy = require("passport-local").Strategy;
+const express = require('express');
+const logger = require('morgan');
+const bodyparser = require('body-parser');
+const models = require('./db/index');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// const Users = models.User;
-// const Workouts = models.Workout;
-// const Exercises = models.Exercise;
-// const MuscleGroups = models.MuscleGroup;
-// const Equipment = models.Equipment;
+const Users = models.User;
+const Workouts = models.Workout;
+const Exercises = models.Exercise;
+const MuscleGroups = models.MuscleGroup;
+const Equipment = models.Equipment;
 
 app.use(express.json());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-passport.use(
-  new LocalStrategy(function (username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username." });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-      return done(null, user);
-    });
-  })
-);
-
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    res.status(200).json({ message: "Hello from Buttercups Server" });
+    res.status(200).json({ message: 'Hello from Buttercups Server' });
   } catch (err) {
     console.error(err);
   }
 });
-
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
