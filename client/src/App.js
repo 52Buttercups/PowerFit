d0b34ddd317a8e1182638cdd73f7cb1bf95f61d8
JessBonanno <jessbonanno@gmail.com>
2021-05-24 19:28:16 -0400

    resolved merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,8 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 // components
 import Welcome from './components/welcome/Welcome';
 import Dashboard from './components/dashboard/Dashboard';
-<<<<<<< HEAD
 import WorkoutViewer from './components/workoutView/WorkoutViewer';
-=======
 import WorkoutBuilder from './components/workoutBuilder/WorkoutBuilder';
->>>>>>> 8221e3592f74c1b4bddcd0d2d915547bb5ee6b42
 
 const theme = createMuiTheme({
   palette: {
--- server/database/data/dummy.js
+++ server/database/data/dummy.js
@@ -1,4 +1,4 @@
-/* 
+/*
 + users (list of users)
   - username
   - password
@@ -29,104 +29,104 @@
   - name
 */
 
-let dummyData = {
+const dummyData = {
   users: {
     1: {
       username: 'testUsername1',
       password: 'testPassword1',
-      favorites: [1]
+      favorites: [1],
     },
     2: {
       username: 'testUsername2',
       password: 'testPassword2',
-      favorites: [2]
-    }
+      favorites: [2],
+    },
   },
   workouts: {
     1: {
       name: 'testWorkout1',
-      exercises: [1, 2, 3]
+      exercises: [1, 2, 3],
     },
     2: {
       name: 'testWorkout2',
-      exercises: [2, 1, 3]
-    }
+      exercises: [2, 1, 3],
+    },
   },
   exercises: {
     1: {
       name: 'squats',
       instructions: 'Stand up with your feet shoulder-width apart. Bend your knees, press your hips back and stop the movement once the hip joint is slightly lower than the knees. Press your heels into the floor to return to the initial position. Repeat until set is complete. PROPER FORM AND BREATHING PATTERN.',
-      video: "youtube.com",
+      video: 'youtube.com',
       muscleGroups: [1],
-      equipment: [5]
+      equipment: [5],
     },
     2: {
       name: 'pushups',
       instructions: '1. Get down on all fours, placing your hands slightly wider than your shoulders. 2. Straighten your arms and legs. 3. Lower your body until your chest nearly touches the floor. 4. Pause, then push yourself back up. 5. Repeat.',
-      video: "youtube.com",
+      video: 'youtube.com',
       muscleGroups: [2, 5, 4],
-      equipment: [2]
+      equipment: [2],
     },
     3: {
       name: 'pullups',
       instructions: 'Grab the pullup bar with your palms down (shoulder-width grip). Hang to the pullup-bar with straight arms and your legs off the floor. Pull yourself up by pulling your elbows down to the floor. Go all the way up until your chin passes the be bar. Lower yourself until your arms are straight.',
-      video: "youtube.com",
+      video: 'youtube.com',
       muscleGroups: [6, 7],
-      equipment: [4]
-    }
+      equipment: [4],
+    },
   },
   muscleGroups: {
     1: {
-      name: "legs",
+      name: 'legs',
     },
     2: {
-      name: "chest",
+      name: 'chest',
     },
     3: {
-      name: "back",
+      name: 'back',
     },
     4: {
-      name: "tricep",
+      name: 'tricep',
     },
     5: {
-      name: "core",
+      name: 'core',
     },
     6: {
-      name: "bicep",
+      name: 'bicep',
     },
     7: {
-      name: "lats"
+      name: 'lats',
     },
     8: {
-      name: "shoulders"
-    }
+      name: 'shoulders',
+    },
   },
   equipment: {
     1: {
-      name: "dumbbells"
+      name: 'dumbbells',
     },
     2: {
-      name: "none"
+      name: 'none',
     },
     3: {
-      name: "treadmill"
+      name: 'treadmill',
     },
     4: {
-      name: "pullup bar"
+      name: 'pullup bar',
     },
     5: {
-      name: "squat rack"
+      name: 'squat rack',
     },
     6: {
-      name: "bench rack"
+      name: 'bench rack',
     },
     7: {
-      name: "kettlebell"
+      name: 'kettlebell',
     },
     8: {
-      name: "medicine ball"
-    }
-  }
+      name: 'medicine ball',
+    },
+  },
 };
 
 module.exports = dummyData;
