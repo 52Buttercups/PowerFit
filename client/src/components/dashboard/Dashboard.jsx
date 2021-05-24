c1867cd28b00480e23e7813e169241058bfdf35b
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-24 17:06:25 -0500

    removes unused styles

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -42,11 +42,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
   typography: {
     padding: '4px 9px',
   },
