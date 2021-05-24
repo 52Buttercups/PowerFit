e8283ccf29ea503f24158d8ebb35c094258097bb
nanaboat <p.nana.boateng@gmail.com>
2021-05-24 16:01:20 -0700

    Workout builder UI
    Completed the layout for the Workout Builder

--- client/src/components/workoutBuilder/WorkoutBuilder.jsx
+++ client/src/components/workoutBuilder/WorkoutBuilder.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Builder from './Builder';
 import styles from './workoutBuilder.module.scss';
 
 const WorkoutBuilder = () => (
   <div className={styles.builderContainer}>
-    <div className={styles.builder} />
+    <div className={styles.builder}>
+      <Builder />
+    </div>
     <div className={styles.builderImage} />
   </div>
 );
