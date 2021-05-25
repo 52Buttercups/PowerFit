2a161927566dc64db8d525ecfba2918c05226b3f
JessBonanno <jessbonanno@gmail.com>
2021-05-25 19:33:23 -0400

    added username to register response

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,10 +1,11 @@
-
 const express = require('express');
+
 const router = express.Router();
 const passport = require('passport');
 
 const models = require('../database/index');
-const {User} = models;
+
+const { User } = models;
 
 router.post('/', (req, res) => {
   // Uses passport-local-mongoose's register function to automatically add a salt and hash
@@ -15,7 +16,7 @@ router.post('/', (req, res) => {
     }
     // Logs the user in once a user has successfully registered
     passport.authenticate('local')(req, res, () => {
-      res.status(201).json({ message: `${req.body.username} has been created.` });
+      res.status(201).json({ message: `${req.body.username} has been created.`, username: req.body.username });
     });
   });
 });
