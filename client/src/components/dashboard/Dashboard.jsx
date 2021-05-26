b9d28bf33a3a06fa7df2f276d1f3d118ab409269
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-26 18:53:03 -0500

    completes toggle faves

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -4,6 +4,9 @@ import { Link, useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 import { makeStyles } from '@material-ui/core/styles';
 
 // components
@@ -11,35 +14,56 @@ import BuilderCard from './BuilderCard';
 import { WorkoutContext } from '../../context/WorkoutContext';
 import { APIContext } from '../../context/APIContext';
 
-const userWorkouts = {
-  id: 1,
-  username: 'john',
-  favorites: [
-    {
-      id: 1,
-      name: 'Arm Day',
-      exercises: [
-        {
-          id: 1,
-          name: 'Perfect pushups',
-          instructions: 'Get down on the floor and push the earth away from yourself like Chuck Norris',
-          video: 'https://www.youtube.com/watch?v=IODxDxX7oi4',
-          muscleGroups: [
-            {
-              id: 1,
-              name: 'bicep',
-            },
-            {
-              id: 2,
-              name: 'core',
-            },
-          ],
-          equipment: null,
-        },
-      ],
-    },
-  ],
-};
+const exampleWorkouts = [
+  {
+    id: 1,
+    name: 'Arm Day',
+    isFavorite: false,
+    exercises: [
+      {
+        id: 1,
+        name: 'Perfect pushups',
+        instructions: 'Get down on the floor and push the earth away from yourself like Chuck Norris',
+        video: 'https://www.youtube.com/watch?v=IODxDxX7oi4',
+        muscleGroups: [
+          {
+            id: 1,
+            name: 'bicep',
+          },
+          {
+            id: 2,
+            name: 'core',
+          },
+        ],
+        equipment: null,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: 'Leg Day',
+    isFavorite: true,
+    exercises: [
+      {
+        id: 1,
+        name: 'Perfect pushups',
+        instructions: 'Get down on the floor and push the earth away from yourself like Chuck Norris',
+        video: 'https://www.youtube.com/watch?v=IODxDxX7oi4',
+        muscleGroups: [
+          {
+            id: 1,
+            name: 'bicep',
+          },
+          {
+            id: 2,
+            name: 'core',
+          },
+        ],
+        equipment: null,
+      },
+    ],
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -66,7 +90,10 @@ const useStyles = makeStyles((theme) => ({
 
 const Dashboard = () => {
   const history = useHistory();
-  const [workouts, setWorkouts] = useState(userWorkouts.favorites);
+  const [workouts, setWorkouts] = useState(exampleWorkouts);
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+
   const { setWorkoutToView } = useContext(WorkoutContext);
   const { getAUsersWorkouts } = useContext(APIContext);
   const styles = useStyles();
@@ -87,6 +114,21 @@ const Dashboard = () => {
     }
   }, []);
 
+  useEffect(() => {
+    // initialize favorites
+    const faves = [];
+    workouts.forEach((workout) => {
+      if (workout.isFavorite === true) {
+        faves.push(workout);
+      }
+    });
+    setFavorites(faves);
+  }, [workouts]);
+
+  const toggleFavorites = () => {
+    setShowFavorites(!showFavorites);
+  };
+
   return (
     <div className={styles.root}>
       <Grid container spacing={3}>
@@ -100,22 +142,32 @@ const Dashboard = () => {
           <Typography color="primary" variant="h4">
             My workouts
           </Typography>
-
-          {workouts.length > 0 && workouts.map((workout) => (
+          <FormControlLabel
+            color="primary"
+            control={
+              <Switch checked={showFavorites} onChange={toggleFavorites} name="showFavorites" color="secondary" />
+            }
+            label="Show Only Favorites"
+          />
+          {showFavorites && favorites.length > 0 && favorites.map((workout) => (
             <div key={workout.id} className={styles.workout}>
               <Typography color="primary" className={styles.typography}>
                 {workout.name}
               </Typography>
-              {/* <Link
-                to={{
-                  // eslint-disable-next-line no-underscore-dangle
-                  pathname: `/viewer/${workout._id}`,
-                  // hash: `${workout.name}`,
-                  // state: { workout },
-                }}
-              > */}
+
               <Button onClick={() => viewWorkout(workout)} color="secondary">View</Button>
-              {/* </Link> */}
+
+            </div>
+          ))}
+
+          {!showFavorites && workouts.length > 0 && workouts.map((workout) => (
+            <div key={workout.id} className={styles.workout}>
+              <Typography color="primary" className={styles.typography}>
+                {workout.name}
+              </Typography>
+
+              <Button onClick={() => viewWorkout(workout)} color="secondary">View</Button>
+
             </div>
           ))}
 
