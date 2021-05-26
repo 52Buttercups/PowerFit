985281e143d279636bca45d2774652fafca167d1
Alex <alex.garay94@gmail.com>
2021-05-26 11:53:35 -0700

    Implemented create-exercise route

--- server/controllers.js
+++ server/controllers.js
@@ -50,9 +50,31 @@ const getWorkoutsByName = (req, res) => {
     });
 };
 
+const createExercise = (req,res) => {
+  models.Exercises.create({
+    name: req.body.name,
+    instructions: req.body.instructions,
+    video: req.body.video,
+    muscleGroups: [{
+      name: req.body.muscleGroups,
+    }],
+    equipment: [{
+      name: req.body.equipment,
+    }],
+  })
+    .then(() => {
+      res.status(201).json({message: `Exercise: "${req.body.name}" has been created.`});
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(401).json({message: `Exercise: "${req.body.name}" has failed to be created.`});
+    });
+};
+
 module.exports = {
   getAllExercises,
   getExercisesByName,
   getAllWorkouts,
   getWorkoutsByName,
+  createExercise,
 };
