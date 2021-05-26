9505ee53e94515745139842dc87e9baf0be4ed67
nanaboat <p.nana.boateng@gmail.com>
2021-05-26 12:52:22 -0700

    Removed userWorkout dummy data

--- server/database/data/dummy.js
+++ server/database/data/dummy.js
@@ -29,7 +29,9 @@
   - name
 */
 
-const { Users, Workouts, Exercises, UserWorkouts } = require('../index');
+const {
+  Users, Workouts, Exercises,
+} = require('../index');
 // const { db } = require('../index');
 
 const userData = [
@@ -203,103 +205,6 @@ const exerciseData = [
       { name: 'legs' },
     ],
     equipment: [{ name: 'squat rack' }],
-  },
-];
-
-const userWorkoutData = [
-  {
-    username: "testUsername1",
-    workouts: [
-      {
-        name: 'testWorkout1',
-        exercises: [
-          {
-            name: 'air squats',
-            instructions: 'Stand up with your feet shoulder-width apart. Bend your knees, press your hips back and stop the movement once the hip joint is slightly lower than the knees. Press your heels into the floor to return to the initial position. Repeat until set is complete. PROPER FORM AND BREATHING PATTERN.',
-            video: 'youtube.com',
-            muscleGroups: [{
-              name: 'legs',
-            }],
-            equipment: [{
-              name: 'none',
-            }],
-          },
-          {
-            name: 'pushups',
-            instructions: '1. Get down on all fours, placing your hands slightly wider than your shoulders. 2. Straighten your arms and legs. 3. Lower your body until your chest nearly touches the floor. 4. Pause, then push yourself back up. 5. Repeat.',
-            video: 'youtube.com',
-            muscleGroups: [{
-              name: 'chest',
-            }, {
-              name: 'core',
-            }, {
-              name: 'tricep',
-            }],
-            equipment: [{
-              name: 'none',
-            }],
-          },
-          {
-            name: 'pullups',
-            instructions: 'Grab the pullup bar with your palms down (shoulder-width grip). Hang to the pullup-bar with straight arms and your legs off the floor. Pull yourself up by pulling your elbows down to the floor. Go all the way up until your chin passes the be bar. Lower yourself until your arms are straight.',
-            video: 'youtube.com',
-            muscleGroups: [{
-              name: 'biceps',
-            }, {
-              name: 'lats',
-            }],
-            equipment: [{
-              name: 'pullup bar',
-            }],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    username: "testUsername2",
-    workouts: [
-      {
-        name: 'testWorkout2',
-        exercises: [
-          {
-            name: 'pushups',
-            instructions: '1. Get down on all fours, placing your hands slightly wider than your shoulders. 2. Straighten your arms and legs. 3. Lower your body until your chest nearly touches the floor. 4. Pause, then push yourself back up. 5. Repeat.',
-            video: 'youtube.com',
-            muscleGroups: [{
-              name: 'chest',
-            }, {
-              name: 'core',
-            }, {
-              name: 'tricep',
-            }],
-            equipment: [{
-              name: 'none',
-            }],
-          },
-          {
-            name: 'air squats',
-            instructions: 'Stand up with your feet shoulder-width apart. Bend your knees, press your hips back and stop the movement once the hip joint is slightly lower than the knees. Press your heels into the floor to return to the initial position. Repeat until set is complete. PROPER FORM AND BREATHING PATTERN.',
-            video: 'youtube.com',
-            muscleGroups: [{
-              name: 'legs',
-            }],
-            equipment: [{
-              name: 'none',
-            }],
-          },
-          {
-            name: 'pullups',
-            instructions: 'Grab the pullup bar with your palms down (shoulder-width grip). Hang to the pullup-bar with straight arms and your legs off the floor. Pull yourself up by pulling your elbows down to the floor. Go all the way up until your chin passes the be bar. Lower yourself until your arms are straight.',
-            video: 'youtube.com',
-            muscleGroups: [
-              { name: 'bicep' },
-              { name: 'lats' }],
-            equipment: [{ name: 'pullup bar' }],
-          },
-        ],
-      },
-    ],
   },
 ];
 
