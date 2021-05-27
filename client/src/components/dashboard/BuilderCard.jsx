8d3fc3eb72ce309f859becd6f5831f2b078e6955
Tom <tomshibley@gmail.com>
2021-05-27 14:31:53 -0700

    Fixed clear workout builder

--- client/src/components/dashboard/BuilderCard.jsx
+++ client/src/components/dashboard/BuilderCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { useHistory } from 'react-router-dom';
+import { WorkoutContext } from '../../context/WorkoutContext';
 
 // image
 import stockImg from '../../assets/boxed-water.jpg';
@@ -24,10 +25,18 @@ const useStyles = makeStyles({
 function BuilderCard() {
   const classes = useStyles();
   const history = useHistory();
+  const { setNewWorkout } = useContext(WorkoutContext);
+  const goToBuilder = () => {
+    setNewWorkout({
+      name: '',
+      exercises: [],
+    });
+    history.push('/builder');
+  };
 
   return (
     <Card className={classes.root}>
-      <CardActionArea onClick={() => history.push('/builder')}>
+      <CardActionArea onClick={goToBuilder}>
         <CardMedia
           component="img"
           className={classes.media}
