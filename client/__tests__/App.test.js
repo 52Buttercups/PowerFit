e649cb008d077c1e1887f2b7332b159997037e37
jyxsul <jonn.sullins@gmail.com>
2021-05-28 00:09:34 -0500

    test if app renders

--- client/__tests__/App.test.js
+++ client/__tests__/App.test.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
-
+describe('App Component', () => {
+  it('Renders App component correctly', () => {
+    const appComponent = render(<App/>);
+    expect(appComponent.exists()).toBe(true);
+  });
+});
