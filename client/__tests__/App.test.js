9d376ac34f8e01740a915b4ee229b2ec53dba0aa
jyxsul <jonn.sullins@gmail.com>
2021-05-28 00:11:11 -0500

    test if app renders

--- client/__tests__/App.test.js
+++ client/__tests__/App.test.js
@@ -4,7 +4,16 @@ import App from '../src/App';
 
 describe('App Component', () => {
   it('Renders App component correctly', () => {
-    const appComponent = render(<App/>);
+    const appComponent = render(<App />);
     expect(appComponent.exists()).toBe(true);
   });
 });
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
